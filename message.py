e5555a82de2a1cdd57e2e3ee5328c6b2e60c5e57
faymek <fcwat163@163.com>
2016-12-20 12:14:54 +0800

    Add files via upload

--- message.py
+++ message.py
@@ -23,6 +23,106 @@ div {
 }
 '''
 
+
+class Dialog:
+    def __init__(self, m):
+        gid, contain = m
+        self.gid = gid
+        self.contain = contain
+
+    def getIcon(self):
+        iconid = int(self.gid) % 4
+        return './head/' + str(iconid) + '.png'
+
+
+class DialogDB:
+    def __init__(self):
+        conn = sqlite3.connect('test.db')
+        self.cu = conn.cursor()
+        self.dialogList = []
+        self.readDialog()
+
+    def readDialog(self):
+        self.dialogList = []
+        self.cu.execute("select gid, contain from people")
+        for row in self.cu:
+            m = Dialog(row)
+            self.dialogList.append(m)
+
+    def searchDialog(self,tags):
+        self.dialogList = []
+
+
+class DialogList(QtGui.QFrame):
+    def __init__(self, parent):
+        QtGui.QFrame.__init__(self, parent)
+        self.setGeometry(0, 60, 255, 540)
+        self.db = DialogDB()
+
+        self.lineedit = QtGui.QLineEdit(self)
+        self.lineedit.setGeometry(0, 0, 255, 35)
+
+        self.lineedit.returnPressed.connect(self._lineedit_returnPressed)
+        self.lineedit.textChanged.connect(self._lineedit_textChanged)
+
+        self.list_view = QtGui.QListView(self)
+        self.list_view.setGeometry(0, 35, 255, 505)
+        self.list_view.setSpacing(3)
+
+        self.list_model = DialogListModel(self.db.dialogList)
+        self.list_view.setModel(self.list_model)
+        self.list_view.setIconSize(QtCore.QSize(50, 50))
+
+
+
+    def _lineedit_textChanged(self, text):
+        print "text changed:", text
+
+        self.item_box.filter_list_by_keyword(text)
+        self.list_view.update()
+
+    def _lineedit_returnPressed(self):
+        text = self.lineedit.text()
+
+        print "return press:", text
+        print "items:", self.item_box.items
+
+
+class DialogListModel(QtCore.QAbstractListModel):
+    def __init__(self, dialogList):
+        super(DialogListModel, self).__init__()
+        self._items = set()
+        for item in dialogList:
+            self._items.add(item)
+        self.items = list(self._items)
+
+    def rowCount(self, parent):
+        return len(self.items)
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if not index.isValid():
+            return None
+
+        item = self.items[index.row()]
+        fullname, icon_path, user_data = item.ccontent, item.getIcon(), item.gid
+
+        if role == QtCore.Qt.DisplayRole:
+            return fullname
+
+        elif role == QtCore.Qt.DecorationRole:
+            icon = QtGui.QIcon(icon_path)
+            return icon
+
+        elif role == QtCore.Qt.BackgroundColorRole:
+            colorTable = [0xD1EE22, 0xFFFFE0, 0xDCDCDC, 0xF0FFFF,
+                          0xD1EEEE, 0xCDCDC1, 0x00FFFF, 0x00FF7F]
+            cc = item.gid % 8
+            color = QtGui.QColor(colorTable[cc])
+            return QtGui.QBrush(color)
+
+        return None
+
+
 class Message:
     def __init__(self, m):
         dnid, bgntime, endtime, content, pointer = m
@@ -61,11 +161,11 @@ class MessageDB:
 class MessageList(QtGui.QFrame):
     def __init__(self, parent):
         QtGui.QFrame.__init__(self, parent)
-        self.setGeometry(0, 60, 1000, 540)
+        self.setGeometry(265, 60, 745, 540)
         self.db = MessageDB()
 
         self.list_view = QtGui.QListView(self)
-        self.list_view.setGeometry(0, 0, 1000, 540)
+        self.list_view.setGeometry(0, 0, 745, 540)
         self.list_view.setSpacing(3)
 
         self.list_model = MessageListModel(self.db.messageList)
@@ -113,11 +213,17 @@ class EmailEditor(QtGui.QFrame):
         QtGui.QFrame.__init__(self, parent)
         self.setGeometry(265, 60, 745, 540)
 
+        self.lineedit = QtGui.QLineEdit(self)
+        self.lineedit.setGeometry(0, 505, 745, 35)
+
+        self.lineedit.returnPressed.connect(self._lineedit_returnPressed)
+        self.lineedit.textChanged.connect(self._lineedit_textChanged)
+
         self.text_doc = QtGui.QTextDocument()
         self.text_doc.setDefaultStyleSheet(css)
 
         self.text_edit = QtGui.QTextEdit(self)
-        self.text_edit.setGeometry(0, 0, 745, 540)
+        self.text_edit.setGeometry(0, 0, 745, 505)
         self.text_edit.setDocument(self.text_doc)
         self.text_edit.setTextInteractionFlags(QtCore.Qt.TextSelectableByKeyboard | QtCore.Qt.TextSelectableByMouse)
         #        self.text_edit.setReadOnly(True)
@@ -170,6 +276,18 @@ class EmailEditor(QtGui.QFrame):
         #        t = self.text_doc.toHtml()
         #        with open('log2.txt', 'w') as f:
         #            f.write(t.toUtf8())
+
+    def _lineedit_textChanged(self, text):
+        print "text changed:", text
+
+        self.item_box.filter_list_by_keyword(text)
+        self.list_view.update()
+
+    def _lineedit_returnPressed(self):
+        text = self.lineedit.text()
+
+        print "return press:", text
+        print "items:", self.item_box.items
 
 
 
